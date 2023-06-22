bfd0a7e0e171f668ce73e757fca2b77bbcd44a5c
Hiren Prajapati <hirenpr.itpath@gmail.com>
2023-06-22 14:53:18 +0530

    Changed response message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Hey this is my API running 🥳");
 });
 
 app.listen(5000, () => {
